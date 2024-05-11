cb7469374aeb2698c7fa0f4b38720067f15597dc
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-05-11 17:59:39 -0300

    Added options.lua
    File containing usefull config for the editor

---
+++ .config/nvim/lua/matt/core/options.lua
@@ -0,0 +1,82 @@
+-- [[ Global Settings ]] --
+
+-- Set <Space> as global leader key
+vim.g.mapleader = " "
+
+-- Show netrw as a file-tree
+-- vim.g.netrw_liststyle = 3
+
+local opt = vim.opt
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+opt.number = true -- Make line numbers default
+opt.relativenumber = true -- Relative line numbers
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = 'yes'
+
+opt.wrap = false
+
+-- Decrease update time
+opt.updatetime = 250
+opt.timeout = true
+opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+opt.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+opt.termguicolors = true
+
+opt.background = "dark"
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+opt.smartindent = true
+
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.scrolloff = 8
+
+opt.updatetime = 250
+
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+opt.termencoding = "utf-8"
+
+-- open new split panes to right and below
+
+opt.splitright = true
+opt.splitbelow = true
+
+-- cursor-line
+opt.cursorline = true
+
+opt.backspace = "indent,eol,start"
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
