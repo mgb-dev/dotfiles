535cad1c3e4f71ed9d99e9b725fcb2b5f613e461
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-05-11 18:53:17 -0300

    Added utility plugins
    Plugins that don't need any configuration to work

---
+++ .config/nvim/lua/matt/plugins/init.lua
@@ -0,0 +1,8 @@
+--[[ Utility plugins]] --
+return {
+  -- NOTE: First, some plugins that don't require any configuration
+  "nvim-lua/plenary.nvim",
+  "tpope/vim-rhubarb",
+  -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-sleuth",
+}
