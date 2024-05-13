81ba6788d60679880b7d34f0e355b630a54d3fe2
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-05-13 12:34:38 -0300

    Added: Mason

---
+++ .config/nvim/lua/matt/plugins/lsp/mason.lua
@@ -0,0 +1,37 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		mason.setup({
+			ui = {
+				icons ={
+					package_installed = "✓",
+					package_pending = "→",
+					package_uninstalled = "✗",
+
+				}
+			}
+		})
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"bashls",
+				"eslint",
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"emmet_ls",
+				"marksman",
+				"jsonls",
+				"lua_ls",
+				"taplo",
+				"gopls",
+			},
+		})
+	end,
+}
