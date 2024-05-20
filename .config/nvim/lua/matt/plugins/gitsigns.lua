711949b8c3c21f5ce3bdc04cbcbd56be2aced72f
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-05-20 07:29:39 -0300

    style(nvim-config): fix format for 'gitsigns.lua'
    change format in the file, following the format rules

--- .config/nvim/lua/matt/plugins/gitsigns.lua
+++ .config/nvim/lua/matt/plugins/gitsigns.lua
@@ -1,15 +1,15 @@
 return {
-	'lewis6991/gitsigns.nvim',
+	"lewis6991/gitsigns.nvim",
 	config = function()
-		require('gitsigns').setup({
+		require("gitsigns").setup({
 			signs = {
-				    add          = { text = '┃' },
-				    change       = { text = '┃' },
-				    delete       = { text = '_' },
-				    topdelete    = { text = '‾' },
-				    changedelete = { text = '~' },
-				    untracked    = { text = '┆' },
-				  },
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
 		})
 	end,
 }
