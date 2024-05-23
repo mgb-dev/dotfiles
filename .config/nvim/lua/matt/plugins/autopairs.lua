24f4de30b339dbad12f0d80c78d5729991fa17b9
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-05-23 05:56:48 -0300

    style(neovim-config): fix format for config files
    'autopairs.lua', 'treesitter.lua'

--- .config/nvim/lua/matt/plugins/autopairs.lua
+++ .config/nvim/lua/matt/plugins/autopairs.lua
@@ -1,18 +1,18 @@
 return {
-	'windwp/nvim-autopairs',
+	"windwp/nvim-autopairs",
 	event = "InsertEnter",
 	opts = {
 		check_ts = true,
 		ts_config = {
-			lua = {"string", "source"},
-			javascript = {"string", "template_string"},
+			lua = { "string", "source" },
+			javascript = { "string", "template_string" },
 			java = false,
 		},
-		disable_filetype = {"TelescopePrompt", "spectre_panel"},
-		fast_wrap =  {
+		disable_filetype = { "TelescopePrompt", "spectre_panel" },
+		fast_wrap = {
 			map = "<M-e>",
-			chars = { "{", "[", "(", "\"", "'" },
+			chars = { "{", "[", "(", '"', "'" },
 			check_comma = true,
-		}
-	} -- this is equalent to setup({}) function
+		},
+	}, -- this is equalent to setup({}) function
 }
--- .config/nvim/lua/matt/plugins/treesitter.lua
+++ .config/nvim/lua/matt/plugins/treesitter.lua
@@ -1,40 +1,40 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = {"BufReadPre", "BufNewFile"},
-  build = "<cmd>TSUpdate<CR>",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = "<cmd>TSUpdate<CR>",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-    treesitter.setup({
-      highlight = {enable = true},
-      indent = {enable = true},
-      autotag = {enable = true},
-      auto_install = true,
-      additional_vim_regex_highlighting = false,
-      ensure_installed = {
-        "bash",
-        "comment",
-        "dot",
-        "jsdoc",
-        "json",
-        "regex",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "toml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "lua",
-        "dockerfile",
-        "gitignore",
-        "vimdoc",
-      },
-    })
-  end,
+		treesitter.setup({
+			highlight = { enable = true },
+			indent = { enable = true },
+			-- autotag = { enable = true },
+			auto_install = true,
+			additional_vim_regex_highlighting = false,
+			ensure_installed = {
+				"bash",
+				"comment",
+				"dot",
+				"jsdoc",
+				"json",
+				"regex",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"toml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"lua",
+				"dockerfile",
+				"gitignore",
+				"vimdoc",
+			},
+		})
+	end,
 }
