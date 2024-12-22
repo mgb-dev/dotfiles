0a353b369a4ccd044d651947e9ae23dd515e3c8d
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-12-22 06:41:05 -0300

    feat(nvim): change harpoon keybindings
    - Now it uses Alt + 1-4 to quick access

--- nvim/.config/nvim/lua/matt/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/matt/plugins/harpoon.lua
@@ -16,17 +16,21 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		map("n", "<C-h>", function()
+		-- Formerly <C-h>
+		map("n", "<M-1>", function()
 			harpoon:list():select(1)
-		end)
-		map("n", "<C-t>", function()
+		end, { desc = "[Alt]ernate to Harpoon [1]st item" })
+		-- Formerly <C-t>
+		map("n", "<M-2>", function()
 			harpoon:list():select(2)
-		end)
-		map("n", "<C-n>", function()
+		end, { desc = "[Alt]ernate to Harpoon [2]nd item" })
+		-- Formerly <C-n>
+		map("n", "<M-3>", function()
 			harpoon:list():select(3)
-		end)
-		map("n", "<C-s>", function()
+		end, { desc = "[Alt]ernate to Harpoon [3]rd item" })
+		--Formerly <C-s>
+		map("n", "<M-4>", function()
 			harpoon:list():select(4)
-		end)
+		end, { desc = "[Alt]ernate to Harpoon [4]th item" })
 	end,
 }
