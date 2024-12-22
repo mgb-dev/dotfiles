1ee078a07679a16b3deb7ee854d6375c41bea4d5
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-12-22 06:37:40 -0300

    feat(nvim): change treesitter version and fixed formatting

--- nvim/.config/nvim/lua/matt/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/matt/plugins/treesitter.lua
@@ -1,41 +1,42 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = "<cmd>TSUpdate<CR>",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  tag = "v0.9.3",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
 
-		treesitter.setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-			-- autotag = { enable = true },
-			auto_install = true,
-			additional_vim_regex_highlighting = false,
-			ensure_installed = {
-				"go",
-				"bash",
-				"comment",
-				"dot",
-				"jsdoc",
-				"json",
-				"regex",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"toml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"lua",
-				"dockerfile",
-				"gitignore",
-				"vimdoc",
-			},
-		})
-	end,
+    treesitter.setup({
+      highlight = { enable = true },
+      indent = { enable = true },
+      -- autotag = { enable = true },
+      auto_install = true,
+      additional_vim_regex_highlighting = false,
+      ensure_installed = {
+        "go",
+        "bash",
+        "comment",
+        "dot",
+        "jsdoc",
+        "json",
+        "regex",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "toml",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "lua",
+        "dockerfile",
+        "gitignore",
+        "vimdoc",
+      },
+    })
+  end,
 }
