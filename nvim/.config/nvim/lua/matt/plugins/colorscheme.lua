c8207d16d63e7598536ecb07acaf0cf71bab5c8d
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-12-22 05:48:22 -0300

    feat(nvim): enable transparent background

--- nvim/.config/nvim/lua/matt/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/matt/plugins/colorscheme.lua
@@ -2,6 +2,13 @@ return {
   "EdenEast/nightfox.nvim",
   priority = 1000,
   config = function()
+    local options = {
+      transparent = true,
+    }
+    require("nightfox").setup({
+      options = options,
+    })
+
     vim.cmd.colorscheme("carbonfox")
   end,
 }
