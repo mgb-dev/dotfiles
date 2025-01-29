2643b10de2e58dde71b3461729a71f24222d7451
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2025-01-29 06:11:05 -0300

    style(nvim): fix white space indentation

--- nvim/.config/nvim/lua/matt/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/matt/plugins/lsp/mason.lua
@@ -1,51 +1,60 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "→",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
+    local mason_nvim_dap = require("mason-nvim-dap")
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "→",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"bashls",
-				"eslint",
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"emmet_ls",
-				"marksman",
-				"jsonls",
-				"lua_ls",
-				"taplo",
-				"gopls",
-			},
-		})
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "bashls",
+        "eslint",
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "emmet_ls",
+        "marksman",
+        "jsonls",
+        "lua_ls",
+        "taplo",
+        "gopls",
+      },
+    })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				--formatters
-				"prettier",
-				"stylua",
-				"gofumpt",
-				"goimports-reviser",
-				"golines",
-				--linters
-				"eslint_d",
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      ensure_installed = {
+        --formatters
+        "prettier",
+        "stylua",
+        "gofumpt",
+        "goimports-reviser",
+        "golines",
+        --linters
+        "eslint_d",
+      },
+    })
+
+    -- Automatically install this debuggers via Mason
+    mason_nvim_dap.setup({
+      ensure_installed = {
+        "delve",
+      },
+    })
+  end,
 }
--- nvim/.config/nvim/lua/matt/plugins/trouble.lua
+++ nvim/.config/nvim/lua/matt/plugins/trouble.lua
@@ -1,15 +1,15 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"folke/todo-comments.nvim",
-	},
-	keys = {
-		{ "<leader>xx", "<cmd>Trouble<CR>", desc = "Open/close trouble list" },
-		{ "<leader>xw", "<cmd>Trouble workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>xd", "<cmd>Trouble document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-		{ "<leader>xq", "<cmd>Trouble quickfix<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>xl", "<cmd>Trouble loclist<CR>", desc = "Open trouble location list" },
-		{ "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
-	},
+  "folke/trouble.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
+  },
+  keys = {
+    { "<leader>xx", "<cmd>Trouble<CR>", desc = "Open/close trouble list" },
+    { "<leader>xw", "<cmd>Trouble workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
+    { "<leader>xd", "<cmd>Trouble document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
+    { "<leader>xq", "<cmd>Trouble quickfix<CR>", desc = "Open trouble quickfix list" },
+    { "<leader>xl", "<cmd>Trouble loclist<CR>", desc = "Open trouble location list" },
+    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+  },
 }
