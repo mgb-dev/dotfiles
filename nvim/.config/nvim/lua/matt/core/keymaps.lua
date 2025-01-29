1603f63402b8a9ace3099720c4c49bbb56f6d9ea
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2025-01-29 06:13:06 -0300

    feat(nvim): add keymaps to config
    - tabnext bindig to change tabs
    - spell toggle

--- nvim/.config/nvim/lua/matt/core/keymaps.lua
+++ nvim/.config/nvim/lua/matt/core/keymaps.lua
@@ -4,11 +4,16 @@ vim.g.mapleader = " "
 local map = vim.keymap.set
 
 -- [[ User Keymaps ]]
-
+map("n", "<leader>tt", "<cmd>tabnext<CR>", { desc = "[T]ab cycle" })
 -- [[ Spell Keymaps]]
 map("n", "<leader>sj", "]s", { desc = "[S]pell next" })
 map("n", "<leader>sk", "[s", { desc = "[S]pell previous" })
 map("n", "<leader>ss", "z=", { desc = "[S]pell [S]ugest" })
+map("n", "<leader>st", function()
+  local win_id = vim.api.nvim_get_current_win()
+  local curr_win = vim.wo[win_id]
+  curr_win.spell = not curr_win.spell
+end, { desc = "[S]pell [T]oggle" })
 
 map("n", "<M-f>", "<C-^>", { desc = "[Alt]ernate [F]ile" })
 map("n", "<leader>ee", vim.cmd.Ex, { desc = "[E]xplorer Open" })
