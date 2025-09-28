2c6b745e5e6fc98e3a8f528c27966066c9fcd583
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2025-09-28 07:41:07 -0300

    style(nvim): fix format to telescope file
    - fix quote format
    - add helping comment

--- nvim/.config/nvim/lua/matt/plugins/telescope.lua
+++ nvim/.config/nvim/lua/matt/plugins/telescope.lua
@@ -19,12 +19,13 @@ return {
     "folke/todo-comments.nvim",
   },
   config = function()
+    -- INFO:please make sure you have the `fd` command installed and in your $PATH
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
     telescope.setup({
       defaults = {
-        path_display = {"smart"},
+        path_display = { "smart" },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous,
@@ -39,17 +40,16 @@ return {
 
     -- [[keymaps]] --
     local map = vim.keymap.set
-    local tel_buil = require('telescope.builtin')
+    local tel_buil = require("telescope.builtin")
 
     map("n", "<leader>ff", tel_buil.find_files, { desc = "[F]ind [F]iles in cwd" })
     map("n", "<leader>fb", tel_buil.buffers, { desc = "[F]ind opened [B]uffers" })
     map("n", "<leader>fr", tel_buil.oldfiles, { desc = "[F]ind [R]ecent files" })
     map("n", "<leader>fk", tel_buil.keymaps, { desc = "[F]ind [K]eymaps" })
     map("n", "<leader>fs", tel_buil.live_grep, { desc = "[F]ind [S]tring in cwd" })
-    map('n', '<leader>fh', tel_buil.help_tags, { desc = '[F]ind [H]elp' })
-    map('n', '<leader>fw', tel_buil.grep_string, { desc = '[F]ind current [W]ord' })
-    map('n', '<leader>fd', tel_buil.diagnostics, { desc = '[F]ind [D]iagnostics' })
+    map("n", "<leader>fh", tel_buil.help_tags, { desc = "[F]ind [H]elp" })
+    map("n", "<leader>fw", tel_buil.grep_string, { desc = "[F]ind current [W]ord" })
+    map("n", "<leader>fd", tel_buil.diagnostics, { desc = "[F]ind [D]iagnostics" })
     map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "[F]ind [T]odos" })
-
   end,
 }
