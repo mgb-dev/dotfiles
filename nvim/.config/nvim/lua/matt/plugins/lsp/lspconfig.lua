f5775ea9aeb8bc718968197a535f6ba36448d365
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2025-10-04 00:01:40 -0300

    feat(nvim): add lazydev plugin
    solves `vim` global missing error

--- nvim/.config/nvim/lua/matt/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/matt/plugins/lsp/lspconfig.lua
@@ -4,13 +4,20 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/neodev.nvim", opts = {} },
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          --         -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
   },
   config = function()
-    local lspconfig = require("lspconfig")
-    local util = require("lspconfig.util")
     local mason_lspconfig = require("mason-lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local map = vim.keymap.set
     local telescope = require("telescope.builtin")
 
